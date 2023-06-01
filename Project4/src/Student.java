df5375b81ce088d23b3c4ebaceeebd962b1f4f47
Shravya Dongala <reddysravya9966@gmail.com>
2023-05-31 21:52:24 -0500

    commit

---
+++ Project4/src/Student.java
@@ -0,0 +1,14 @@
+
+public class Student {
+ String Name;
+ public void setName(String Name) {
+	 this.Name=Name;
+ }
+ public String getName() {
+	 return Name;
+ }
+	public static void main(String[] args) {
+	Student S = new Student();
+	S.setName("Shravya Reddy");
+	System.out.println(S.getName());
+	}}
